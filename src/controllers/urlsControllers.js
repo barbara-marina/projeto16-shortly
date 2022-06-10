d65d433a8d73fe772ed36a3d6b63e6a03cacbb22
barbarina <barbaramarinaufsm@gmail.com>
2022-06-10 08:08:24 -0300

    feat: add get url and short url by url id endpoint

--- src/controllers/urlsControllers.js
+++ src/controllers/urlsControllers.js
@@ -20,3 +20,22 @@ export async function shortenUrls(req, res) {
         return res.sendStatus(500);
     }
 }
+
+export async function getUrlsById(req, res) {
+    const { id } = req.params;
+
+    try {
+        const verifyUrl = await db.query (`
+            SELECT urls.id, urls."shortUrl", urls.url
+            FROM urls
+            WHERE urls.id=$1;
+        `, [id]);
+
+        if (verifyUrl.rowCount === 0) return res.status(404).send("Url id does not found.");
+
+        res.status(200).send(verifyUrl.rows[0]);
+    } catch (e) {
+        console.log(e);
+        return res.sendStatus(500);
+    }
+}
--- src/routes/urlsRouter.js
+++ src/routes/urlsRouter.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { shortenUrls } from "../controllers/urlsControllers.js";
+import { getUrlsById, shortenUrls } from "../controllers/urlsControllers.js";
 import schemasValidations from "../middlewares/schemasValidations.js";
 import tokenMiddleware from "../middlewares/tokenValidationMiddleware.js";
 import urlSchema from "../schemas/urlSchema.js";
@@ -7,5 +7,6 @@ import urlSchema from "../schemas/urlSchema.js";
 const urlsRouter = Router();
 
 urlsRouter.post("/urls/shorten", tokenMiddleware,schemasValidations(urlSchema), shortenUrls);
+urlsRouter.get("/urls/:id", getUrlsById);
 
 export default urlsRouter;
